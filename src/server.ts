4e92c019b23616ed89cd9c68c5d26f3d95d92ae9
Willian Hercilio Pezzini <williantijucas@gmail.com>
2025-07-02 06:17:31 -0300

    Gerado o prisma Client para o Backend

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import  Express  from "express";
+import cors from "cors";
 import routes from "./routes";
+
 
 const server = Express();
 
+server.use(cors())
 server.use(Express.json());
 
 server.use(routes);
 
-server.listen("4000");
+server.listen(4000, () => {
+    console.log("Servidor rodando na porta 4000");
+});
